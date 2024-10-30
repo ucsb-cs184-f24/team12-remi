d025f5f85f3aefd851e24fb27dcec39dc6b09e37
Alexander Chau <apchau@ucsb.edu>
2024-10-30 14:37:08 -0700

    make stack header hidden, friends_list, and auth's header name be home

--- remi-app/app/(auth)/register.tsx
+++ remi-app/app/(auth)/register.tsx
@@ -1,24 +1,39 @@
-import React, { useState } from 'react';
-import { TouchableWithoutFeedback, Keyboard, View, TextInput, Button, ActivityIndicator, StyleSheet, Alert } from 'react-native';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { FirebaseError } from 'firebase/app';
+import React, { useState } from "react";
+import {
+  TouchableWithoutFeedback,
+  Keyboard,
+  View,
+  TextInput,
+  Button,
+  ActivityIndicator,
+  StyleSheet,
+  Alert,
+} from "react-native";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { FirebaseError } from "firebase/app";
 
-import { auth, db } from '../../firebaseConfig'; 
-import { doc, setDoc, getDocs, collection, query, where } from 'firebase/firestore';
-
+import { auth, db } from "../../firebaseConfig";
+import {
+  doc,
+  setDoc,
+  getDocs,
+  collection,
+  query,
+  where,
+} from "firebase/firestore";
 
 export default function Register() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
   const [loading, setLoading] = useState(false);
 
   const checkIfUsernameExists = async (username: string) => {
-    const usersRef = collection(db, 'RemiUsers');
-    const q = query(usersRef, where('username', '==', username));
+    const usersRef = collection(db, "RemiUsers");
+    const q = query(usersRef, where("username", "==", username));
     const querySnapshot = await getDocs(q);
 
-    return !querySnapshot.empty;  // Returns true if the username exists
+    return !querySnapshot.empty; // Returns true if the username exists
   };
 
   const signUp = async () => {
@@ -26,28 +41,34 @@ export default function Register() {
     try {
       const usernameExists = await checkIfUsernameExists(username);
       if (usernameExists) {
-        alert('Username is already taken. Please choose a different one.');
+        alert("Username is already taken. Please choose a different one.");
         setLoading(false);
         return;
       }
 
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const user = userCredential.user;
 
-      await setDoc(doc(db, 'RemiUsers', user.uid), {
+      await setDoc(doc(db, "RemiUsers", user.uid), {
         username: username,
         email: email,
-        friend_list: [],
-        visibility: "private"
+        friends_list: [],
+        visibility: "private",
       });
 
-      alert('Account created successfully!');
+      alert("Account created successfully!");
     } catch (e: any) {
       const err = e as FirebaseError;
-      if (err.code === 'auth/email-already-in-use') {
-        alert('This email is already associated with an account. Please use a different email or login with this existing email.');
+      if (err.code === "auth/email-already-in-use") {
+        alert(
+          "This email is already associated with an account. Please use a different email or login with this existing email."
+        );
       } else {
-        alert('Registration failed: ' + err.message);
+        alert("Registration failed: " + err.message);
       }
     } finally {
       setLoading(false);
@@ -57,39 +78,39 @@ export default function Register() {
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-        autoCorrect={false}
-        placeholderTextColor='#BCD5AC'
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-        autoCorrect={false}
-        keyboardType="email-address"
-        autoCapitalize="none"
-        placeholderTextColor='#BCD5AC'
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        autoCorrect={false}
-        secureTextEntry
-        placeholderTextColor='#BCD5AC'
-      />
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          value={username}
+          onChangeText={setUsername}
+          autoCorrect={false}
+          placeholderTextColor="#BCD5AC"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+          autoCorrect={false}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          placeholderTextColor="#BCD5AC"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          value={password}
+          onChangeText={setPassword}
+          autoCorrect={false}
+          secureTextEntry
+          placeholderTextColor="#BCD5AC"
+        />
 
-      {loading ? (
-        <ActivityIndicator size={'small'} style={{ margin: 28 }} />
-      ) : (
-        <Button title="Create Account" onPress={signUp} color="#0D5F13" />
-      )}
+        {loading ? (
+          <ActivityIndicator size={"small"} style={{ margin: 28 }} />
+        ) : (
+          <Button title="Create Account" onPress={signUp} color="#0D5F13" />
+        )}
       </View>
     </TouchableWithoutFeedback>
   );
@@ -99,7 +120,7 @@ const styles = StyleSheet.create({
   container: {
     padding: 20,
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: "center",
   },
   input: {
     marginVertical: 10,
@@ -107,7 +128,7 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderRadius: 4,
     padding: 10,
-    backgroundColor: '#fff',
-    borderColor: '#0D5F13',
-  }
+    backgroundColor: "#fff",
+    borderColor: "#0D5F13",
+  },
 });
