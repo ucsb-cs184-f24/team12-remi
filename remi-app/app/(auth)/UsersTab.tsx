0d49e3576c1c63c9f7aa22d4ff1383e72ed329f4
apple <qwu132@ucsb.edu>
2024-11-16 14:44:59 -0800

    search user ui fixed

--- remi-app/app/(auth)/UsersTab.tsx
+++ remi-app/app/(auth)/UsersTab.tsx
@@ -189,6 +189,7 @@ const UsersTab: React.FC<UsersTabProps> = ({ searchQuery }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: "100%",
     backgroundColor: "#FFF9E6",
   },
   item: {
